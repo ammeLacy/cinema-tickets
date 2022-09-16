1bb191eb1ed63b7d9fe028023892714e14aa2b5b
EmmaLacy <emma_lacy@hotmail.com>
2022-09-16 18:39:45 +0100

    Refactor: move check for number of params to separate method

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -5,15 +5,21 @@ export default class TicketService {
   /**
    * Should only have private methods other than the one below.
    */
+  
+  _areSufficientParams(ticketTypeRequests) {
+    if (ticketTypeRequests.length === 0) {
+      throw new InvalidPurchaseException('Insufficient arguements');
+    }
+  }
 
   purchaseTickets(accountId, ...ticketTypeRequests) {
     // throws InvalidPurchaseException
     
-    if (ticketTypeRequests.length === 0) {
-      throw new InvalidPurchaseException('Insufficient arguements');
-    }
+    this._areSufficientParams(ticketTypeRequests);
     if (!Number.isInteger(accountId) ||!accountId || accountId < 1 ) {
       throw new InvalidPurchaseException('Invalid account id');
     }
   }
+
+ 
 }
