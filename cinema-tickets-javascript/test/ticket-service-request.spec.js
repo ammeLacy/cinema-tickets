c7f2b888f8586618ecebc21f9b711a54e452f1f6
EmmaLacy <emma_lacy@hotmail.com>
2022-09-16 19:05:30 +0100

    Refactor: move tests to same order as class

--- cinema-tickets-javascript/test/ticket-service-request.spec.js
+++ cinema-tickets-javascript/test/ticket-service-request.spec.js
@@ -3,6 +3,12 @@ import TicketTypeRequest from '../src/pairtest/lib/TicketTypeRequest';
 
 describe('TicketService', () => {
   describe('PurchaseTickets', () => {
+    it('should throw an error if less than 2 arguements are passed to it', () => {
+      const service = new TicketService();
+      expect(() => {
+        service.purchaseTickets(new TicketTypeRequest('ADULT', 5));
+      }).toThrow('Insufficient arguements');
+    });
     it('should throw an error if account ID is undefined', () => {
       const service = new TicketService();
       expect(() => {
@@ -21,11 +27,7 @@ describe('TicketService', () => {
         service.purchaseTickets('1', new TicketTypeRequest('ADULT', 5));
       }).toThrow('Invalid account id');
     });
-    it('should throw an error if less than 2 arguements are passed to it', () => {
-      const service = new TicketService();
-      expect(() => {
-        service.purchaseTickets(new TicketTypeRequest('ADULT', 5));
-      }).toThrow('Insufficient arguements');
-    });
+    
+ 
   });
 });
