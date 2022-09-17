afdd5dd912d7cc09a97aef9eb9906a84cf2d578e
EmmaLacy <emma_lacy@hotmail.com>
2022-09-17 13:25:08 +0100

    Test: payment service acalled with accountId and correct payment

--- cinema-tickets-javascript/test/ticket-service-request.spec.js
+++ cinema-tickets-javascript/test/ticket-service-request.spec.js
@@ -7,21 +7,6 @@ jest.mock('../src/thirdparty/paymentgateway/TicketPaymentService')
 beforeEach(() => {
   TicketPaymentService.mockClear();
 });
-/*
-import SoundPlayer from './sound-player';
-import SoundPlayerConsumer from './sound-player-consumer';
-jest.mock('./sound-player'); // SoundPlayer is now a mock constructor
-
-beforeEach(() => {
-  // Clear all instances and calls to constructor and all methods:
-  SoundPlayer.mockClear();
-});
-
-it('We can check if the consumer called the class constructor', () => {
-  const soundPlayerConsumer = new SoundPlayerConsumer();
-  expect(SoundPlayer).toHaveBeenCalledTimes(1);
-});
-*/
 
 describe('TicketService', () => {
   describe('PurchaseTickets', () => {
@@ -105,4 +90,3 @@ describe('TicketService', () => {
       });
     });
   });
-
